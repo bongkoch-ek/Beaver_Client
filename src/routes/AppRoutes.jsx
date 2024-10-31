ad52fc49ccd8e68b07ffe8e1f44ce94c48f77a65
forture-G <gorakod1234@gmail.com>
2024-10-31 12:17:41 +0700

    add erroPage

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,15 +4,19 @@ import Register from "../pages/register";
 import Login from "../pages/login";
 import UserLayout from "../layouts/UserLayout";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <UserLayout />, // ใช้ UserLayout สำหรับเส้นทางที่มี Navbar
+    element: <>
+    <UserLayout /> 
+    </>,
     children: [
       { path: "home", element: <Home /> },
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
+      { path: "*", element: <ErrorPage /> },
     ],
   },
 ]);
