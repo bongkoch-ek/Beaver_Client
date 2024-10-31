d4a24dd3ff6a149129d96dd145ed321ee78b8de7
Wanapong-dev <wanapong64@gmail.com>
2024-10-31 15:52:14 +0700

    navbarupdate

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,8 +1,14 @@
 // src/components/Navbar.js
 import { BeaverLogo } from "@/src/icons";
-import React from "react";
+import React, { useState } from "react";
+
 
 const Navbar = () => {
+
+const [user,setUser] = useState(false)
+
+
+
   return (
     <div className="flex bg-gray-100 p-8">
       {/* Navbar */}
@@ -18,27 +24,65 @@ const Navbar = () => {
               <span className="text-[#FFE066] font-bold text-xl">Beaver</span>
             </div>
 
-            <a href="#home" className="hover:underline hover:text-yellow-400">
+            <a href="/home" className="hover:underline hover:text-yellow-400">
               Home
             </a>
             <a
-              href="#project"
+              href="/project"
               className="hover:underline hover:text-yellow-400"
             >
               Project
             </a>
           </div>
+
+          {
+            user ? (
+              <div className="relative bg-neutral-900">
+                <button
+                  onClick={toggleDropdown}
+                  className="flex items-center justify-between gap-2
+                          hover:text-yellow-400 hover:scale-105 hover:-translate-y-1 hover:duration-200"
+                >
+                  <img
+                    src={"https://cdn-icons-png.flaticon.com/512/6858/6858504.png"}
+                    className="w-8 h-8"
+                    alt="Profile Icon"
+                  />
+                  Profile
+                  <IoIosArrowDown />
+                </button>
+                {isOpen && (
+                  <ul className="absolute right-0 mt-2 w-48 bg-neutral-800 rounded-lg z-10 shadow-lg">
+                    <li
+                      onClick={hdlNavigate}
+                      className="py-2 px-3 cursor-pointer rounded-sm hover:bg-neutral-700 hover:duration-200 active:bg-green-400"
+                    >
+                      Setting
+                    </li>
+                    <li
+                      onClick={hdlLogout}
+                      className="py-2 px-3 cursor-pointer rounded-sm hover:bg-neutral-700 hover:duration-200 active:bg-green-400"
+                    >
+                      LogOut
+                    </li>
+                  </ul>
+                )}
+              </div>
+            ) :
           <div className="flex space-x-8 text-gray-700">
             <a
-              href="#register"
+              href="/register"
               className="hover:underline hover:text-yellow-400"
             >
               Register
             </a>
-            <a href="#login" className="hover:underline hover:text-yellow-400">
+            <a href="/login" className="hover:underline hover:text-yellow-400">
               Login
             </a>
           </div>
+          }
+
+
         </div>
       </div>
     </div>
