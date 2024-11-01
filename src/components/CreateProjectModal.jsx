8f36e14f35f57cd1a6ab85ce41951c9d4c859ac5
forture-G <gorakod1234@gmail.com>
2024-11-02 01:51:22 +0700

    edit createModal(ver02)

--- src/components/CreateProjectModal.jsx
+++ src/components/CreateProjectModal.jsx
@@ -24,35 +24,28 @@ const CreateProjectModal = () => {
         </Button>
 
       {/* <Dialog  open={isOpen} onOpenChange={setIsOpen}> */} 
-      <Dialog className="flex justify-center items-center" open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent className=" flex items-center justify-around rounded-lg gap-16 ">
+      <Dialog  open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-lg gap-16 h-[400px] max-w-3xl">
 
-            
-            <div className="flex flex-col gap-6 p-8  items-center justify-center border-2 border-gray-200 rounded-lg">
+            <div className="flex flex-col gap-6 p-8 items-center justify-center border-2 border-gray-200 rounded-lg h-[300px]">
               <div className='bg-gray-400 w-[120px] h-[120px] flex justify-center items-center rounded-md'>
                 Pic
               </div>
-              {/* <Label htmlFor="uploadImage" className="block text-sm font-medium text-gray-700 ">
-                Upload Image
-              </Label> */}
               <Input type="file" id="uploadImage" className="w-full border border-gray-300 p-2 rounded-md" />
             </div>
 
-            {/* ซีกขวา: Input Email และปุ่ม Create Project */}
-            <div className="flex flex-col  justify-center items-center">
-              <div className="grid w-full max-w-sm items-center ">
-              <Label htmlFor="email" className=" text-sm font-medium text-[#333333] mb-1 flex">
-                Project Name
-              </Label>
-              <Input id="email" type="email" placeholder="Type your project" className="w-full text-[#C0C0C0] border border-gray-300 p-2 rounded-md " />
+            <div className="flex flex-col justify-center items-center h-[300px] gap-8">
+              <div className="grid w-full max-w-sm items-center">
+                <Label htmlFor="email" className="text-sm font-medium text-[#333333] mb-1 flex">
+                  Project Name
+                </Label>
+                <Input id="email" type="email" placeholder="Type your project" className="w-full text-[#C0C0C0] border border-gray-300 p-2 rounded-md" />
               </div>
-              <div className='flex'>
-              <DialogFooter className="flex justify-center items-center mt-4">
+              <DialogFooter>
                 <Button onClick={handleCreateProject} className="bg-[#ffe066] text-black px-4 py-2 rounded hover:bg-yellow-400">
                   Create New Project
                 </Button>
-              </DialogFooter> 
-              </div>
+              </DialogFooter>
             </div>
         </DialogContent>
       </Dialog>
