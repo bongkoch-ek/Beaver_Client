b1c273c2f3d15007e6f89706e902dae534f0b2fb
jay43733 <tossapon43733@gmail.com>
2024-11-01 15:39:08 +0700

    Almost Finished Profile and Update Primary Button, Secondary Button

--- src/components/common/SecondaryButton.jsx
+++ src/components/common/SecondaryButton.jsx
@@ -2,28 +2,25 @@ import React from "react";
 
 export default function SecondaryButton({
   text,
-  func,
   type,
   isClicked,
   color,
   Icon,
   ...rest
 }) {
-
   return (
     <div>
       <button
         type={type}
         {...rest}
-        onClick={func}
-        className={`border-2
+        className={`border-2 border-[#767676]
       ${
         isClicked == text
-          ? "bg-[#7A5C61] duration-300 text-white font-bold"
-          : "bg-white border-[#91959A] text-[#91959A] hover:bg-[#ECF1F6] hover:font-semibold border-none items-center "
+          ? "bg-[#FFE066] duration-300 text-white font-semibold"
+          : "bg-white border-[#767676] text-[#767676] hover:bg-[#ECF1F6] font-semibold hover:border-[#91959a] "
       }
            text-[16px] ${color} font-normal text-center
-           px-4 py-2 w-full rounded-[20px] min-w-[40px]`}
+           px-4 py-2 w-full rounded-[8px] min-w-[40px]`}
       >
         <div className="flex items-center justify-center gap-4">
           {Icon && <Icon />}
