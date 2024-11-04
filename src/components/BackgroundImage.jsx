d455fe889ecb0e78e0a4e554870dc5ef8667b8ee
jay43733 <tossapon43733@gmail.com>
2024-11-04 15:48:52 +0700

    Edit bgImage

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SecondaryButton from "./common/SecondaryButton";
 import { Plus } from "lucide-react";
+import bgImage from "../../../../pic/bg-image.png"
 
 const BackgroundImage = ({ isDisabled }) => {
   return (
@@ -10,6 +11,7 @@ const BackgroundImage = ({ isDisabled }) => {
           isDisabled ? "" : "hover:bg-[#00000055]"
         } w-full h-[120px] relative rounded-tl-[32px] rounded-tr-[32px] transition-all duration-200`}
       >
+        <img src={bgImage} alt="bgImage" className="w-full h-full"  />
         <div
           className={`absolute inset-0 flex justify-center items-start top-1/4 opacity-0 ${
             isDisabled
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import { BeaverLogo, HidePasssword } from "../icons";
 import Input from "../components/common/Input";
 import validate from "../utils/validator";
@@ -7,47 +7,36 @@ import useUserStore from "../stores/userStore";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [input, setInput] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    displayName: '',
-    password: '',
-    confirmPassword: ''
-  })
-  const [formErrors, setFormErrors] = useState({})
+    firstname: "",
+    lastname: "",
+    email: "",
+    displayName: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
 
-<<<<<<< HEAD
-  const [showPassword, setShowPassword] = useState(true);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(true);
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-=======
-  const actionRegister = useUserStore( state => state.actionRegister ) 
->>>>>>> origin/develop
+  const actionRegister = useUserStore((state) => state.actionRegister);
 
-  const hdlChange = e => {
-    setInput(prv => ({ ...prv, [e.target.name]: e.target.value }))
+  const hdlChange = (e) => {
+    setInput((prv) => ({ ...prv, [e.target.name]: e.target.value }));
     const { [e.target.name]: _, ...newData } = formErrors;
-    setFormErrors(newData)
-  }
+    setFormErrors(newData);
+  };
 
   const hdlRegister = async (e) => {
-    e.preventDefault()
-    const error = validate.validateRegister(input)
+    e.preventDefault();
+    const error = validate.validateRegister(input);
     if (error) {
-      setFormErrors(error)
-      console.log(error)
-      return
+      setFormErrors(error);
+      console.log(error);
+      return;
     }
     // await actionRegister(input)
-    navigate("/home")
-  }
+    navigate("/home");
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
@@ -59,9 +48,7 @@ const Register = () => {
               Create Account
             </h2>
             <div className="flex items-center gap-4">
-              <BeaverLogo
-                className="w-[62px] h-[62px] rounded-full"
-              />
+              <BeaverLogo className="w-[62px] h-[62px] rounded-full" />
               <h1 className="text-[#ffe066] text-5xl font-semibold font-['IBM Plex Sans Thai'] leading-[64px]">
                 Beaver
               </h1>
@@ -74,13 +61,28 @@ const Register = () => {
               Account Info
             </h3>
             <div className="grid grid-cols-2 gap-6">
-              <div >
-                <Input label="First name" placeholder="First name" name="firstname" value={input.firstname} onChange={hdlChange} isError={formErrors.firstname ? true : false} errMessage={formErrors.firstname || ""}/>
+              <div>
+                <Input
+                  label="First name"
+                  placeholder="First name"
+                  name="firstname"
+                  value={input.firstname}
+                  onChange={hdlChange}
+                  isError={formErrors.firstname ? true : false}
+                  errMessage={formErrors.firstname || ""}
+                />
               </div>
               <div>
-                <Input label="Last name" placeholder="Last name" name="lastname" value={input.lastname} onChange={hdlChange} isError={formErrors.lastname ? true : false} errMessage={formErrors.lastname || ""}/>
+                <Input
+                  label="Last name"
+                  placeholder="Last name"
+                  name="lastname"
+                  value={input.lastname}
+                  onChange={hdlChange}
+                  isError={formErrors.lastname ? true : false}
+                  errMessage={formErrors.lastname || ""}
+                />
               </div>
-
             </div>
           </div>
 
@@ -91,17 +93,50 @@ const Register = () => {
             </h3>
             <div className="grid grid-cols-2 gap-6">
               <div>
-                <Input label="Display name" placeholder="Display name" name="displayName" value={input.displayName} onChange={hdlChange} isError={formErrors.displayName ? true : false} errMessage={formErrors.displayName || ""}/>
+                <Input
+                  label="Display name"
+                  placeholder="Display name"
+                  name="displayName"
+                  value={input.displayName}
+                  onChange={hdlChange}
+                  isError={formErrors.displayName ? true : false}
+                  errMessage={formErrors.displayName || ""}
+                />
               </div>
               <div>
-                <Input label="Email" placeholder="Email" name="email" value={input.email} onChange={hdlChange} isError={formErrors.email ? true : false} errMessage={formErrors.email || ""}/>
+                <Input
+                  label="Email"
+                  placeholder="Email"
+                  name="email"
+                  value={input.email}
+                  onChange={hdlChange}
+                  isError={formErrors.email ? true : false}
+                  errMessage={formErrors.email || ""}
+                />
               </div>
               <div className="relative w-full">
-                <Input type="password" label="Password" placeholder="Password" name="password" value={input.password} onChange={hdlChange} isError={formErrors.password ? true : false} errMessage={formErrors.password || ""}/>
-
+                <Input
+                  type="password"
+                  label="Password"
+                  placeholder="Password"
+                  name="password"
+                  value={input.password}
+                  onChange={hdlChange}
+                  isError={formErrors.password ? true : false}
+                  errMessage={formErrors.password || ""}
+                />
               </div>
               <div className="relative w-full">
-                <Input type="password" label="Confirm Password" placeholder="Confirm password" name="confirmPassword" value={input.confirmPassword} onChange={hdlChange} isError={formErrors.confirmPassword ? true : false} errMessage={formErrors.confirmPassword || ""}/>
+                <Input
+                  type="password"
+                  label="Confirm Password"
+                  placeholder="Confirm password"
+                  name="confirmPassword"
+                  value={input.confirmPassword}
+                  onChange={hdlChange}
+                  isError={formErrors.confirmPassword ? true : false}
+                  errMessage={formErrors.confirmPassword || ""}
+                />
               </div>
             </div>
           </div>
