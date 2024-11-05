1f9df4f80b7c6dd7a9763d7ba412a442d047944a
jay43733 <tossapon43733@gmail.com>
2024-11-05 15:29:10 +0700

    Finish Feature Drag and Drop Task without database

--- src/components/DropTaskIndicator.jsx
+++ src/components/DropTaskIndicator.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const DropTaskIndicator = () => {
+const DropTaskIndicator = ({ beforeId, column }) => {
+  // console.log(id, "IDDDDDDDDDDDD")
+  // console.log(column, "Cloummmmmmmmmm")
   return (
-    <div className="my-1 h-1 w-full bg-[#5DB9F8] rounded-lg opacity-0"></div>
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="h-1 w-full bg-[#55A8E2] rounded-lg opacity-0 transition-opacity duration-100"
+    ></div>
   );
 };
 
