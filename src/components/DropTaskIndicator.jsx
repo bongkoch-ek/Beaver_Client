ce466f3af600d34fced3a195b7d4e75754454b48
jay43733 <tossapon43733@gmail.com>
2024-11-05 09:32:57 +0700

    Add DropIndicator

---
+++ src/components/DropTaskIndicator.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DropTaskIndicator = () => {
+  return (
+    <div className="my-1 h-1 w-full bg-[#5DB9F8] rounded-lg opacity-0"></div>
+  );
+};
+
+export default DropTaskIndicator;
