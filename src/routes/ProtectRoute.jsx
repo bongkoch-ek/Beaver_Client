1171e22a635811ce3f9f8598b0ab3e8785c52de3
Pisek-K <pisekkidsawanon@gmail.com>
2024-11-11 16:07:59 +0700

    update add member

--- src/routes/ProtectRoute.jsx
+++ src/routes/ProtectRoute.jsx
@@ -2,28 +2,39 @@ import React, { useEffect, useState } from "react";
 import useUserStore from "../stores/userStore";
 import { currentUser } from "../services/UserService";
 import LoadingToRedirect from "./LoadingToRedirect";
+import { Navigate } from "react-router-dom";
 
 
 const ProtectRoute = ({ element }) => {
-  const [ok, setOk] = useState(false);
+  const [ok, setOk] = useState(null);
   const token = useUserStore((state) => state.token);
   const user = useUserStore((state) => state.user);
   useEffect(() => {
     try {
         if(user && token){
-            setOk(true)
-        }
-        else{
             currentUser(token).then((res) => {
                 setOk(true)
             })
+        }
+        else{
+            setOk(false)
         }
     } catch (err) {
         setOk(false);
         console.log(err)
     }
   }, []);
-  return ok ? element : <LoadingToRedirect />;
+
+  if(ok === null){
+    return <div>Loading...</div>
+}
+
+if(!ok){
+    return <LoadingToRedirect />
+}
+
+return element
+//   return ok ? element : <LoadingToRedirect />;
 };
 
 export default ProtectRoute;
