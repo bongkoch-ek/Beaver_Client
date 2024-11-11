310e7bfc962edd5e2231dc42927363df970d6c85
Wanapong-dev <wanapong64@gmail.com>
2024-11-11 20:12:34 +0700

    editprojectdetail

--- src/pages/Projectpage/ProjectDetailPage.jsx
+++ src/pages/Projectpage/ProjectDetailPage.jsx
@@ -1,38 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import useDashboardStore from "@/src/stores/dashboardStore";
 import useUserStore from "@/src/stores/userStore";
 import ProjectDetail from "@/src/components/ProjectDetail";
 import ProjectTask from "@/src/components/ProjectTask";
-import {getProjectById} from  "../../services/DashboardService"
+import ProjectSchedule from "../../pages/Projectpage/ProjectSchedule";
+import { getProjectById } from "../../services/DashboardService";
+import ProjectDashboard from "./ProjectDashboard";
 
 export default function ProjectDetailPage() {
-  const { projectId } = useParams(); // รับ projectId จาก URL
+  const { projectId } = useParams();
   const token = useUserStore((state) => state.token);
-  const [fetchProject, setFetchProject] = useState([])
-
-  
-
+  const [fetchProject, setFetchProject] = useState([]);
+  const [activeTab, setActiveTab] = useState("task");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await getProjectById( token,projectId);
-        setFetchProject(response); 
+        const response = await getProjectById(token, projectId);
+        setFetchProject(response);
       } catch (error) {
         console.error("Error fetching project data:", error);
       }
     }
 
     fetchData();
-  }, [])
+  }, []);
 
-
+  const renderContent = () => {
+    switch ( activeTab ) {
+      case "task":
+        return <ProjectTask />;
+      case "dashboard":
+        return <ProjectDashboard />;
+      case "schedule":
+        return <ProjectSchedule />;
+      default:
+        return <ProjectTask />;
+    }
+  };
 
   return (
     <div>
-      <ProjectDetail fetchProject={fetchProject}  />
-      <ProjectTask />
+      <ProjectDetail fetchProject={fetchProject} activeTab={activeTab} setActiveTab={setActiveTab} />
+      {renderContent()}
     </div>
   );
 }
