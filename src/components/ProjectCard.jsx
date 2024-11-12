f50d260247f1d62d9918e32c0983d556e998056f
Wanapong-dev <wanapong64@gmail.com>
2024-11-12 21:19:16 +0700

    recenly

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ProjectImg2 } from "../icons";
+import { Link } from "react-router-dom";
 
 export default function ProjectCard(props) {
 
@@ -19,21 +20,23 @@ export default function ProjectCard(props) {
   const late = project.project.list?.filter(item => item.status === "LATE").length;
 
   return (
-    <div className='bg-white rounded-xl p-4 w-60 h-64 hover:shadow-md hover:-translate-y-1'>
+    <div className='bg-white border rounded-xl p-4 w-60 h-64 hover:shadow-md hover:-translate-y-1'>
 
       <div className='flex gap-2 pb-3'>
         <div className='w-7 h-7'>
         <ProjectImg2 className="" />
         </div>
-        <p className='font-semibold text-xl'>{project.project?.projectName}</p>
+        <p className='font-semibold text-xl whitespace-nowrap overflow-hidden text-ellipsis w-[80%]'>
+          {sliceStr(project.project?.projectName || '')}
+        </p>
       </div>
 
       <div className='flex flex-col gap-3'>
        
         <div className='flex justify-between items-center'>
           <p>In progress</p>
-          <div className='bg-[#FFE066] rounded-full w-9 h-9 items-center flex justify-center'>
-            <p className=' font-medium text-[#333333]'>{inprogress}</p>
+          <div className='bg-[#5DB9F8] rounded-full w-9 h-9 items-center flex justify-center'>
+            <p className=' font-medium text-white'>{inprogress}</p>
           </div>
         </div>
         <div className='flex justify-between items-center'>
@@ -49,8 +52,10 @@ export default function ProjectCard(props) {
             <p className=' font-medium text-white'>{late}</p>
           </div>
         </div>
+        
+        <Link to={`/project/${project.project?.id}`} className='text-end items-end hover:underline pt-4'> <p>Continue</p> </Link>
 
-        <button className='text-end items-end hover:underline'> <p>Continue &ndash;&gt;</p> </button>
+
       </div>
 
 
